78b5dd213564b13a7d3f877a06f9674615239632
高一轩 <yixuangao@outlook.com>
2016-01-19 09:59:32 +0800

    完成分割过程

--- SegmentLibrary/SPImage.cpp
+++ SegmentLibrary/SPImage.cpp
@@ -4,15 +4,86 @@ int SPImage::SpSegment(LPCTSTR lpszPathName, int left, int top, int right, int b
 {
 	mSegWidth = right - left;
 	mSegHeight = bottom - top;
-	// load image
+	// 加载图像
 	Log("load image...");
 	int loadResult = LoadOriginalImage(lpszPathName);
 	if (SEG_RESULT_LOAD_SUCCESS != loadResult)
 	{
 		return loadResult;
 	}
-	mSegData = new float[mSegWidth*mSegHeight*MAX_COLOR_DIM];
-	mSegMark = new int[mSegWidth*mSegHeight];
+	InitSpace();
+	// 初始化分割数据
+	Log("initialize segmentation...");
+	for (int i = 0; i < mSegHeight; i++)
+	{
+		for (int j = 0; j < mSegWidth; j++)
+		{
+			for (int k = 0; k < MAX_COLOR_DIM; k++)
+			{
+				mSegData[(i*mSegWidth + j) * MAX_COLOR_DIM + k] = mImgData[((top + i)*mWidth + left + j) * MAX_COLOR_DIM + k];
+			}
+		}
+	}
+	// 预标记
+	for (int i = 0; i < mSegHeight; i++)
+	{
+		for (int j = 0; j < mSegWidth; j++)
+		{
+			if (isCenter(i,j, mSegHeight, mSegWidth))
+			{
+				mSegMark[i*mSegWidth + j] = MARK_FOREGROUND;
+			}
+			else
+			{
+				mSegMark[i*mSegWidth + j] = MARK_BACKGROUND;
+			}
+		}
+	}
+	// 建立图结构
+	Lattice();
+	// 计算运行时间
+	clock_t t1,t2,t10,t11,t12,t13,t14;
+	t1 = clock();
+	Log("-----Start of Segmentation-----\n");
+	for (int i = 0; i < MAX_ITERATIONS; i++)
+	{
+		t10 = clock();
+		Modeling();
+		t11 = clock();
+		Log("modeling cost:{1,0:F4}s", i + 1, (t11 - t10) / 1000);
+		UpdateWeights();
+		t12 = clock();
+		Log("updateWeigths cost:{1,0:F4}s", i + 1, (t12 - t11) / 1000);
+		Dijkstra(0);
+		Dijkstra(1);
+		t13 = clock();
+		Log("dijkstra cost:{1,0:F4}s", i + 1, (t13 - t12) / 1000);
+		Mark();
+		t14 = clock();
+		Log("mark cost:{1,0:F4}s", i + 1, (t14 - t13) / 1000);
+		Log("No.{0,0:D} iteration total cost:{1,0:F4}s\n",i+1,(t14-t11)/1000);
+	}
+	// 将兴趣区域的标记转移到全图上面
+	for (int i = 0; i < mSegHeight; i++)
+	{
+		for (int j = 0; j < mSegWidth; j++)
+		{
+			mImgMark[(top + i)*mWidth + left + j] = mSegMark[i*mSegWidth + j];
+		}
+	}
+	GenerateImage();
+	t2 = clock();
+	Log("-----End of Segmentation-----");
+	Log("segmentation total cost:{0,0:F4}s\n", (t2 - t1) / 1000);
+	ReleaseSpace();
+	return 0;
+}
+
+bool SPImage::InitSpace()
+{
+	int pointCount = mSegWidth*mSegHeight;
+	mSegData = new float[pointCount*MAX_COLOR_DIM];
+	mSegMark = new int[pointCount];
 
 	mMeansBG = new float*[BG_GAUSS_COUNT];
 	for (int i = 0; i < BG_GAUSS_COUNT; i++)
@@ -37,65 +108,47 @@ int SPImage::SpSegment(LPCTSTR lpszPathName, int left, int top, int right, int b
 		mVarFG[i] = new float[MAX_COLOR_DIM];
 	}
 	mLogwFG = new float[FG_GAUSS_COUNT];
-	// initialize segmentation
-	Log("initialize segmentation...");
-	for (int i = 0; i < mSegHeight; i++)
+
+	try
 	{
-		for (int j = 0; j < mSegWidth; j++)
-		{
-			mSegData[(i*mSegWidth + j) * MAX_COLOR_DIM + 0] = mImgData[((top + i)*mWidth + left + j) * MAX_COLOR_DIM + 0];
-			mSegData[(i*mSegWidth + j) * MAX_COLOR_DIM + 1] = mImgData[((top + i)*mWidth + left + j) * MAX_COLOR_DIM + 1];
-			mSegData[(i*mSegWidth + j) * MAX_COLOR_DIM + 2] = mImgData[((top + i)*mWidth + left + j) * MAX_COLOR_DIM + 2];
+		weights.resize(pointCount);
+		for (int i = 0; i < pointCount; i++) {
+			weights[i].resize(10);
 		}
 	}
-	for (int i = 0; i < mSegHeight; i++)
+	catch (std::bad_alloc)
 	{
-		for (int j = 0; j < mSegWidth; j++)
+		return false;
+	}
+	try
+	{
+		edge.resize(pointCount);
+		for (int i = 0; i < pointCount; i++)
 		{
-			if (isCenter(i,j, mSegHeight, mSegWidth))
-			{
-				mSegMark[i*mSegWidth + j] = MARK_FOREGROUND;
-			}
-			else
-			{
-				mSegMark[i*mSegWidth + j] = MARK_BACKGROUND;
-			}
+			edge[i].resize(8);
 		}
 	}
-	// timing
-	clock_t t1,t2,t10,t11,t12,t13,t14;
-	t1 = clock();
-	Log("-----Start of Segmentation-----\n");
-	for (int i = 0; i < MAX_ITERATIONS; i++)
+	catch (std::bad_alloc)
 	{
-		t10 = clock();
-		Modeling();
-		t11 = clock();
-		Log("modeling cost:{1,0:F4}s", i + 1, (t11 - t10) / 1000);
-		//Lattice();
-		t12 = clock();
-		//Log("lattice cost:{1,0:F4}s", i + 1, (t12 - t11) / 1000);
-		//Dijkstra();
-		t13 = clock();
-		//Log("dijkstra cost:{1,0:F4}s", i + 1, (t13 - t12) / 1000);
-		Mark();
-		t14 = clock();
-		Log("mark cost:{1,0:F4}s", i + 1, (t14 - t13) / 1000);
-		Log("No.{0,0:D} iteration total cost:{1,0:F4}s\n",i+1,(t14-t11)/1000);
+		return false;
 	}
-	// 将兴趣区域的标记转移到全图上面
-	for (int i = 0; i < mSegHeight; i++)
+	try
 	{
-		for (int j = 0; j < mSegWidth; j++)
+		distances.resize(pointCount);
+		for (int i = 0; i < pointCount; i++)
 		{
-			mImgMark[(top + i)*mWidth + left + j] = mSegMark[i*mSegWidth + j];
+			distances[i].resize(2);
 		}
 	}
-	GenerateImage();
-	t2 = clock();
-	Log("-----End of Segmentation-----");
-	Log("segmentation total cost:{0,0:F4}s\n", (t2 - t1) / 1000);
-	Log("release space...");
+	catch (std::bad_alloc)
+	{
+		return false;
+	}
+	return true;
+}
+
+void SPImage::ReleaseSpace()
+{
 	if (mImgData != NULL)
 	{
 		delete[] mImgData;
@@ -105,7 +158,6 @@ int SPImage::SpSegment(LPCTSTR lpszPathName, int left, int top, int right, int b
 		delete[] mImgMark;
 	}
 	mImgOriginal.Destroy();
-	return 0;
 }
 
 int SPImage::LoadOriginalImage(LPCTSTR lpszPathName)
@@ -140,14 +192,16 @@ int SPImage::LoadOriginalImage(LPCTSTR lpszPathName)
 		{
 			float R, G, B;
 			float L = 0.0, a = 0.0, b = 0.0;
-			B = bits[j * MAX_COLOR_DIM];
-			G = bits[j * MAX_COLOR_DIM + 1];
-			R = bits[j * MAX_COLOR_DIM + 2];
+			B = bits[j * 3];
+			G = bits[j * 3 + 1];
+			R = bits[j * 3 + 2];
 			RGB2Lab(R, G, B, L, a, b);
 
 			mImgData[(i*mWidth + j) * MAX_COLOR_DIM + 0] = L;
 			mImgData[(i*mWidth + j) * MAX_COLOR_DIM + 1] = a;
 			mImgData[(i*mWidth + j) * MAX_COLOR_DIM + 2] = b;
+			mImgData[(i*mWidth + j) * MAX_COLOR_DIM + 3] = i;
+			mImgData[(i*mWidth + j) * MAX_COLOR_DIM + 4] = j;
 		}
 		bits += pitch;
 	}
@@ -200,14 +254,123 @@ void SPImage::Modeling()
 	delete[] bgpoint;
 	delete[] fgpoint;
 }
-
 /// 计算图结构
+/*************
+ * 7 0 1     
+ * 6   2
+ * 5 4 3
+ * edge8:FG
+ * edge9:BG
+ *************/
 void SPImage::Lattice()
 {
-
-	
+	for (int i = 0; i < mSegHeight; i++)
+	{
+		for (int j = 0; j < mSegWidth; j++)
+		{
+			int index = i*mSegWidth + j;
+			edge[index][0] = Edge(i - 1, j);
+			edge[index][1] = Edge(i - 1, j + 1);
+			edge[index][2] = Edge(i, j + 1);
+			edge[index][3] = Edge(i + 1, j + 1);
+			edge[index][4] = Edge(i + 1, j);
+			edge[index][5] = Edge(i + 1, j - 1);
+			edge[index][6] = Edge(i, j - 1);
+			edge[index][7] = Edge(i - 1, j - 1);
+			
+			for (int k = 0; k < 8; k++)
+			{
+				// 有效边则计算权值
+				if (edge[index][k] != -1)
+				{
+					weights[index][k] = EulerDistance(&mSegData[index * MAX_COLOR_DIM], &mSegData[edge[index][k] * MAX_COLOR_DIM]);
+				}
+				// 标记无效边
+				else
+				{
+					weights[index][k] = 1e30;
+				}
+			}
+		}
+	}
 }
+// 更新到高斯模型的距离
+void SPImage::UpdateWeights()
+{
+	for (int i = 0; i < mSegHeight; i++)
+	{
+		for (int j = 0; j < mSegWidth; j++)
+		{
+			int index = i*mSegWidth + j;
+			weights[index][8] =	GaussDistance(1,&mSegData[index * MAX_COLOR_DIM]);
+			weights[index][9] = GaussDistance(0,&mSegData[index * MAX_COLOR_DIM]);
+		}
+	}
+}
+// 计算最短路径
+// label = 0 到前景源点的最短路径
+// label = 1 到背景汇点的最短路径
+void SPImage::Dijkstra(const int label)
+{
+	int targetIndex;
+	if (label == 0)
+	{
+		targetIndex = 8;
+	}
+	else
+	{
+		targetIndex = 9;
+	}
+	int pointCount = mSegHeight*mSegWidth;
 
+	struct fibheap *tmpHeap = fh_makekeyheap();
+	fibheap_el ** GraphMinDistances;
+	GraphMinDistances = new fibheap_el*[pointCount];
+
+	bool* isVisited = new bool[pointCount];
+
+	for (int i = 0; i < pointCount; i++)
+	{
+		GraphMinDistances[i] = fh_insertkey(tmpHeap, weights[i][targetIndex], (void *)i);
+		isVisited[i] = false;
+	}
+	int position;
+	float minDistance;
+	for (int h = 1; h < pointCount; h++)
+	{
+		minDistance = fh_minkey(tmpHeap);
+		position = (int)fh_extractmin(tmpHeap);
+
+		isVisited[position] = true;
+		distances[position][label] = minDistance;
+
+		long int tmpPoint;
+		float tmpDis;
+		for (int i = 0; i < 8; i++)
+		{
+			tmpPoint = edge[position][i];
+			if ((tmpPoint != -1) && !isVisited[tmpPoint])
+			{
+				tmpDis = weights[position][i];
+				tmpDis += minDistance;
+				fh_replacekey(tmpHeap, GraphMinDistances[tmpPoint], tmpDis);
+			}
+		}
+	}
+	minDistance = fh_minkey(tmpHeap);
+	position = (int)fh_extractmin(tmpHeap);
+	distances[position][label] = minDistance;
+
+	if (isVisited != NULL)
+	{
+		delete[] isVisited;
+	}
+	if (tmpHeap != NULL)
+	{
+		delete[] tmpHeap;
+	}
+}
+// 生成结果
 void SPImage::GenerateImage()
 {
 	int pitch = mImgOriginal.GetPitch();
@@ -221,23 +384,23 @@ void SPImage::GenerateImage()
 		{
 			if (mImgMark[i*mWidth+j] == MARK_FOREGROUND)
 			{
-				binaryBits[j * MAX_COLOR_DIM + 0] = 255;
-				binaryBits[j * MAX_COLOR_DIM + 1] = 255;
-				binaryBits[j * MAX_COLOR_DIM + 2] = 255;
+				binaryBits[j * 3 + 0] = 255;
+				binaryBits[j * 3 + 1] = 255;
+				binaryBits[j * 3 + 2] = 255;
 
-				overlayBits[j * MAX_COLOR_DIM + 0] = originalBits[j * MAX_COLOR_DIM + 0];
-				overlayBits[j * MAX_COLOR_DIM + 1] = originalBits[j * MAX_COLOR_DIM + 1];
-				overlayBits[j * MAX_COLOR_DIM + 2] = originalBits[j * MAX_COLOR_DIM + 2];
+				overlayBits[j * 3 + 0] = originalBits[j * 3 + 0];
+				overlayBits[j * 3 + 1] = originalBits[j * 3 + 1];
+				overlayBits[j * 3 + 2] = originalBits[j * 3 + 2];
 			}
 			else
 			{
-				binaryBits[j * MAX_COLOR_DIM + 0] = 0;
-				binaryBits[j * MAX_COLOR_DIM + 1] = 0;
-				binaryBits[j * MAX_COLOR_DIM + 2] = 0;
+				binaryBits[j * 3 + 0] = 0;
+				binaryBits[j * 3 + 1] = 0;
+				binaryBits[j * 3 + 2] = 0;
 
-				overlayBits[j * MAX_COLOR_DIM + 0] = originalBits[j * MAX_COLOR_DIM + 0] / 3 + 120;
-				overlayBits[j * MAX_COLOR_DIM + 1] = originalBits[j * MAX_COLOR_DIM + 1] / 3 + 120;
-				overlayBits[j * MAX_COLOR_DIM + 2] = originalBits[j * MAX_COLOR_DIM + 2] / 3 + 120;
+				overlayBits[j * 3 + 0] = originalBits[j * 3 + 0] / 3 + 120;
+				overlayBits[j * 3 + 1] = originalBits[j * 3 + 1] / 3 + 120;
+				overlayBits[j * 3 + 2] = originalBits[j * 3 + 2] / 3 + 120;
 			}
 		}
 		originalBits += pitch;
@@ -248,7 +411,7 @@ void SPImage::GenerateImage()
 	mImgBinary.Save(_T("..\\result\\binary.jpg"), Gdiplus::ImageFormatJPEG);
 	mImgOverlay.Save(_T("..\\result\\overlay.jpg"), Gdiplus::ImageFormatJPEG);
 }
-
+// 测试图像
 void SPImage::Test()
 {
 
@@ -261,47 +424,28 @@ void SPImage::Test()
 		{
 			if (mImgMark[i*mWidth + j] == MARK_FOREGROUND)
 			{
-				bits[j * MAX_COLOR_DIM + 0] = 255;
-				bits[j * MAX_COLOR_DIM + 1] = 255;
-				bits[j * MAX_COLOR_DIM + 2] = 255;
+				bits[j * 3 + 0] = 255;
+				bits[j * 3 + 1] = 255;
+				bits[j * 3 + 2] = 255;
 			}
 			else
 			{
-				bits[j * MAX_COLOR_DIM + 0] = 255;
-				bits[j * MAX_COLOR_DIM + 1] = 255;
-				bits[j * MAX_COLOR_DIM + 2] = 255;
+				bits[j * 3 + 0] = 255;
+				bits[j * 3 + 1] = 255;
+				bits[j * 3 + 2] = 255;
 			}
 		}
 		bits += pitch;
 	}
 	mImgTest.Save(_T("..\\result\\test.jpg"), Gdiplus::ImageFormatJPEG);
 }
-/// 标记各个像素点的属性
+// 标记各个像素点的属性
 void SPImage::Mark()
 {
 	int pointCount = mSegHeight*mSegWidth;
 	for (int i = 0; i < pointCount; i++)
 	{
-		float distanceFG = 1e30;
-		float diatanceBG = 1e30;
-		for (int j = 0; j < BG_GAUSS_COUNT; j++)
-		{
-			float D = GaussDistance(0, &mSegData[i * MAX_COLOR_DIM], j);
-			if (D < diatanceBG)
-			{
-				diatanceBG = D;
-			}
-		}
-		
-		for (int j = 0; j < FG_GAUSS_COUNT; j++)
-		{
-			float D = GaussDistance(1, &mSegData[i * MAX_COLOR_DIM], j);
-			if (D < distanceFG)
-			{
-				distanceFG = D;
-			}
-		}
-		if (distanceFG > diatanceBG)
+		if (distances[i][0] > distances[i][1])
 		{
 			mSegMark[i] = MARK_BACKGROUND;
 		}
@@ -311,28 +455,90 @@ void SPImage::Mark()
 		}
 	}
 }
-/// 计算到高斯混合模型的距离
-/// type 0:背景 1:前景
-float SPImage::GaussDistance(int type,float z[3],int k)
+float SPImage::GaussDistance(int label, float z[5])
 {
-	float D;
-	if (type == 0)
+	if (label == 0)
 	{
-		D = -mLogwBG[k] + 0.5*(log(mVarBG[k][0] * mVarBG[k][1] * mVarBG[k][2]) +
-			(z[0] - mMeansBG[k][0])*(z[0] - mMeansBG[k][0]) / mVarBG[k][0] +
-			(z[1] - mMeansBG[k][1])*(z[1] - mMeansBG[k][1]) / mVarBG[k][1] +
-			(z[2] - mMeansBG[k][2])*(z[2] - mMeansBG[k][2]) / mVarBG[k][2]);
+		float diatanceFG = 1e30;
+		for (int j = 0; j < FG_GAUSS_COUNT; j++)
+		{
+			float D = GaussDistance(1, z, j);
+			if (D < diatanceFG)
+			{
+				diatanceFG = D;
+			}
+		}
+		return diatanceFG;
 	}
 	else
 	{
-		D = -mLogwFG[k] + 0.5*(log(mVarFG[k][0] * mVarFG[k][1] * mVarFG[k][2]) +
-			(z[0] - mMeansFG[k][0])*(z[0] - mMeansFG[k][0]) / mVarFG[k][0] +
-			(z[1] - mMeansFG[k][1])*(z[1] - mMeansFG[k][1]) / mVarFG[k][1] +
-			(z[2] - mMeansFG[k][2])*(z[2] - mMeansFG[k][2]) / mVarFG[k][2]);
+		float diatanceBG = 1e30;
+		for (int j = 0; j < BG_GAUSS_COUNT; j++)
+		{
+			float D = GaussDistance(0, z, j);
+			if (D < diatanceBG)
+			{
+				diatanceBG = D;
+			}
+		}
+		return diatanceBG;
+	}
+}
+// 计算到高斯混合模型的距离
+// label 0:前景 1:背景
+float SPImage::GaussDistance(int label,float z[5],int k)
+{
+	float D;
+	if (label == 0)
+	{
+		float D1 = 0;
+		float D2 = 0;
+		for (int i = 0; i < MAX_COLOR_DIM; i++)
+		{
+			D1 += mVarFG[k][i];
+			D2 += (z[i] - mMeansFG[k][i])*(z[i] - mMeansFG[k][i]) / mVarFG[k][i];
+		}
+		D = -mLogwFG[k] + 0.5*(log(D1) + D2);
+	}
+	else
+	{
+		float D1 = 0;
+		float D2 = 0;
+		for (int i = 0; i < MAX_COLOR_DIM; i++)
+		{
+			D1 += mVarBG[k][i];
+			D2 += (z[i] - mMeansBG[k][i])*(z[i] - mMeansBG[k][i]) / mVarBG[k][i];
+		}
+		D = -mLogwBG[k] + 0.5*(log(D1) + D2);
 	}
 	return  D;
 }
-
+// 计算对应坐标的邻接边的index
+int SPImage::Edge(int h, int w)
+{
+	// 点在上边界或左边界
+	if (h < 0 || w < 0)
+	{
+		return -1;
+	}
+	// 点在右边界或下边界
+	else if (h == mSegHeight||w == mSegWidth)
+	{
+		return -1;
+	}
+	return h * mSegWidth + w;
+}
+// 计算两点之间的欧式距离
+float SPImage::EulerDistance(float z1[3], float z2[3])
+{
+	float D = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		D += (z1[i] - z2[i])*(z1[i] - z2[i]);
+	}
+	return D;
+}
+// RGB 到 Lab颜色空间的转换
 void RGB2Lab(float R, float G, float B, float &L, float &a, float &b)
 {
 	L = 0.2126 * R + 0.7152 * G		+ 0.0722 * B;
--- SegmentLibrary/SPImage.h
+++ SegmentLibrary/SPImage.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include <time.h>
 #include "Gmm.h"
+#include "Fibonacci/fib.h"
+#include "Fibonacci/fibpriv.h"
 
 using namespace System;
 /// transfer the rgb space to lab space
@@ -14,7 +16,7 @@ class SPImage
 {
 public:
 	// parameter
-	const int MAX_COLOR_DIM = 3;	// 颜色特征维度
+	const int MAX_COLOR_DIM = 5;	// 颜色特征维度
 	const int MAX_ITERATIONS = 3;	// 最大迭代次数
 	const int BG_GAUSS_COUNT = 5;	// 背景高斯成分个数
 	const int FG_GAUSS_COUNT = 5;	// 前景高斯成分个数
@@ -50,13 +52,25 @@ private:
 	float** mMeansBG;  //背景高斯模型的均值
 	float** mVarBG;    //背景高斯模型的方差
 	float* mLogwBG;    //背景高斯模型权值的log值
+				   
+	std::vector < std::vector <int> > edge;			// edge数组存放图中的所有边
+	std::vector < std::vector <float> > weights;	// 存放图中所有边的权值
+
+	std::vector < std::vector <float> > distances;	// 存放图中所有点到前景源点和背景汇点的距离
 
 private:
 	int LoadOriginalImage(LPCTSTR lpszPathName);
+	bool InitSpace();
+	void ReleaseSpace();
 	void Modeling();
 	void Lattice();
+	void UpdateWeights();
+	int Edge(int h,int w);
+	void Dijkstra(const int label);
 	void Mark();
 	void GenerateImage();
 	void Test();
-	float GaussDistance(int type, float z[3],int k);		// 计算到高斯模型的距离
+	float GaussDistance(int label, float z[5]);					// 计算到高斯模型的距离
+	float GaussDistance(int label, float z[5],int k);			// 计算到高斯模型的距离
+	float EulerDistance(float z1[3],float z2[3]);				// 计算点与点之间的欧式距离
 };
